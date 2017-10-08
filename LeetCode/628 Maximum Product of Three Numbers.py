040c0a1e40e332dbdd039371b3b6ac8c9453adcf
ZJUyuchen <767021727@qq.com>
2017-10-08 17:08:04 +0800

    add 121, 27, 35, 53

--- LeetCode/628 Maximum Product of Three Numbers.py
+++ LeetCode/628 Maximum Product of Three Numbers.py
@@ -4,11 +4,13 @@ Created on Thu Sep 28 11:24:02 2017
 
 @author: yc
 """
-
+#先排序，要么是最大的三个正数相乘，
+#要么是两个最小的负数和最大的正数相乘
 class Solution(object):
     def maximumProduct(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        
+        nums.sort()
+        return max(nums[0]*nums[1]*nums[-1], nums[-1]*nums[-2]*nums[-3])
