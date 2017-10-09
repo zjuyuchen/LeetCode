c7cf03d1ba69afff8cfb3bda70ab14412ee29090
ZJUyuchen <767021727@qq.com>
2017-10-09 23:24:44 +0800

    add 345 459

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -6,15 +6,18 @@ Created on Wed Sep 27 23:59:28 2017
 """
 #Thanks StefanPochmann, origin from his idea.
 #Go through all the possible time.
-class Solution(object):
-    def readBinaryWatch(self, num):
-        """
-        :type num: int
-        :rtype: List[str]
-        """
-        result = []
-        for i in range(12):
-            for j in range(60):
-                if (bin(i)+bin(j)).count('1') == num:
-                    result.append('%d:%02d' % (i, j))
-        return result
+#class Solution(object):
+#    def readBinaryWatch(self, num):
+#        """
+#        :type num: int
+#        :rtype: List[str]
+#        """
+#        result = []
+#        for i in range(12):
+#            for j in range(60):
+#                if (bin(i)+bin(j)).count('1') == num:
+#                    result.append('%d:%02d' % (i, j))
+#        return result
+s = 'Hello'
+s[1] = 'd'
+print(s[1])
