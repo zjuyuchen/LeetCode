1dbc50cefa52e0db567589fa9580309ca11f203e
ZJUyuchen <2010wangzhen@zju.edu.cn>
2017-10-11 16:31:06 +0800

    edit test

--- LeetCode/test.py
+++ LeetCode/test.py
@@ -2,7 +2,13 @@
 #b = [1, 2, 3]
 #for i , z in enumerate(zip(a,b)):
 #    print(i,z)
-s1 = [1,2,3]
-s2 = s1[:]
-s2.pop()
-print(s1,s2)
+#s1 = [1,2,3]
+#s2 = s1[:]
+#s2.pop()
+#print(s1,s2)
+a = '1110'
+b = list(a)
+c = [1,1,1,0]
+print(a,b)
+print(all(a),all(b), all(c))
+print(any(a),any(b), any(c))
