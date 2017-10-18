e1778067e1a19321584efdf376a6b1c1984272d1
ZJUyuchen <2010wangzhen@zju.edu.cn>
2017-10-18 23:20:48 +0800

    add 530 637 669 ignore

---
+++ LeetCode/637. Average of Levels in Binary Tree.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 22:55:27 2017
+
+@author: yc
+"""
+
+#用一个数组纪录二叉树的深度，和每个深度对应的所有vals
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+
+    def averageOfLevels(self, root):
+        info = []
+        def dfs(node, depth = 0):
+            if node:
+                if len(info) <= depth:
+                    info.append([0, 0])
+                info[depth][0] += node.val
+                info[depth][1] += 1
+                dfs(node.left, depth + 1)
+                dfs(node.right, depth + 1)
+        dfs(root)
+
+        return [s/float(c) for s, c in info]
--- LeetCode/test.py
+++ LeetCode/test.py
@@ -1,22 +1,14 @@
-#a = ['aa', 'abd', 'ac']
-#b = [1, 2, 3]
-#for i , z in enumerate(zip(a,b)):
-#    print(i,z)
-#s1 = [1,2,3]
-#s2 = s1[:]
-##s2.pop()
-##print(s1,s2)
-#a = '1110'
-#b = list(a)
-#c = [1,1,1,0]
-#print(a,b)
-#print(all(a),all(b), all(c))
-##print(any(a),any(b), any(c))
-#a  = 'abcd'
-#b = 'abcd'
-#print(a*2)
-#dic = {'a':2}
-#dic.pop('a')
-#print(dic)
-a = [1,2,3]
-print(a[:2])
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 18 23:14:40 2017
+
+@author: yc
+"""
+
+a = []
+def add(a):
+    x = 'x'
+    a.append(x)
+print(a)
+add(a)
+print(a)
