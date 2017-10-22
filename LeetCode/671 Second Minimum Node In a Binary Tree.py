a2c17e63dcfa25e194013e0782032710b7c0dcf2
zjuyuchen <2010wangzhen@zju.edu.cn>
2017-10-22 09:56:37 +0800

    add 671

---
+++ LeetCode/671 Second Minimum Node In a Binary Tree.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  8 00:45:21 2017
+
+@author: Administrator
+"""
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def findSecondMinimumValue(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root.left == None:
+            return -1
+        elif:
+            
+        
